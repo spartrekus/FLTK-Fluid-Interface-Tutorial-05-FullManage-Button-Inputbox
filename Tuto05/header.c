f1f2e1d5c6cb5afe632bb42e2cfcffef47fe08bb
spartrekus <spartrekus@gmail.com>
2017-08-29 23:21:42 +0200

    Add files via upload

---
+++ Tuto05/header.c
@@ -0,0 +1,16 @@
+
+////////////////////////
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h> 
+#include <ctype.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <unistd.h>  
+#include <time.h>
+////////////////////////
+
+
+
